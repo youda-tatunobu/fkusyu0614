10e0b3cf556a99a3265cdae8ff782d35939a9139
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-06-14 13:21:23 +0900

    up

--- fkusyu0614/Form1.cs
+++ fkusyu0614/Form1.cs
@@ -12,9 +12,46 @@ namespace fkusyu0614
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
+            
+
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if(label1.Left<0)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Left>255)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+
+            if (label1.Top > 255)
+            {
+                vy = -vy;
+            }
+
+
         }
     }
 }
